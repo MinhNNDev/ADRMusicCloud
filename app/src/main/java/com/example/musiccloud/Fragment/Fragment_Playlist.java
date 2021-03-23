caf1df71f29a04594302703f96c3553322f18805
banhtrung <hontap12275@gmail.com>
2021-03-23 13:08:45 +0700

    Init Project

---
+++ app/src/main/java/com/example/musiccloud/Fragment/Fragment_Playlist.java
@@ -0,0 +1,34 @@
+package com.example.musiccloud.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.musiccloud.R;
+
+public class Fragment_Playlist extends Fragment {
+    ListView lvplsylist;
+    TextView txttitleplaylist, txtviewxemthemplaylist;
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_playlist, container, false);
+        lvplsylist = view.findViewById(R.id.listviewplaylist);
+        txttitleplaylist = view.findViewById(R.id.textviewtitleplaylist);
+        txtviewxemthemplaylist = view.findViewById(R.id.textviewmoreplaylist);
+        GetData();
+        return view;
+    }
+
+    private void GetData() {
+    }
+}
